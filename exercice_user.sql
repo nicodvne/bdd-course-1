351fe135ffd5391a6b4a771e1bb7dace61bd9b05
Nicolas Davenne <nico.dvne@gmail.com>
2024-05-29 23:07:22 +0200

    add exercise user

---
+++ exercice_user.sql
@@ -0,0 +1,15 @@
+-- Exercice 1 : Création d'un nouvel utilisateur
+
+-- Exercice 2 : Attribution de permissions globales
+
+-- Exercice 3 : Attribution de permissions au niveau de la base de données nommée 'ma_base_de_donnees'
+
+-- Exercice 4 : Attribution de permissions au niveau de la table  nommée 'ma_table'
+
+-- Exercice 5 : Révocation de permissions
+
+-- Exercice 6 : Suppression d'un utilisateur
+
+-- Exercice 7 : Création d'un nouvel utilisateur avec des privilèges de lecture, ajout et modification
+
+-- Exercice 8 : Révocation de toutes les permissions
