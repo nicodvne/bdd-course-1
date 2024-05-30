fc920b4ed2b37eefaf840beba8d614e90e58253c
Nicolas Davenne <nico.dvne@gmail.com>
2024-05-30 14:12:45 +0200

    exercice fucntion

---
+++ exercice_function.sql
@@ -0,0 +1,26 @@
+-- Exercice 1 : Fonction de Conversion de Température
+
+-- Créez une fonction appelée celsius_to_fahrenheit.
+-- La fonction doit prendre un paramètre de type FLOAT représentant une température en Celsius.
+-- La fonction doit retourner la température correspondante en Fahrenheit.
+-- Fahrenheit=(Celsius×9/5)+32
+
+-----------------------------------------------------
+-- Exercice 2 :  Fonction de Calcul de Factorielle
+
+-- Créez une fonction appelée factorielle.
+-- La fonction doit prendre un paramètre de type INT représentant un nombre entier.
+-- La fonction doit retourner la factorielle du nombre.
+-- rappel :  La factorielle de n (notée n!) est le produit de tous les entiers positifs inférieurs ou égaux à n.
+
+
+-----------------------------------------------------
+-- Exercice 3 :   Fonction de Vérification de Nombre Premier
+
+-- Créez une fonction appelée est_premier.
+-- La fonction doit prendre un paramètre de type INT représentant un nombre entier.
+-- La fonction doit retourner TRUE si le nombre est premier, sinon FALSE.
+
+-- rappel :  Un nombre premier est un nombre entier supérieur à 1 qui n'a pas d'autres diviseurs que 1 et lui-même.
+
+
